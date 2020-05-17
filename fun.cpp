a3fc989f580f679613492c3593aff125357a5a39
Oscar <tonyzhao1314@gmail.com>
2020-05-17 21:37:16 +1000

    Add files via upload

---
+++ fun.cpp
@@ -0,0 +1,10 @@
+
+#include<bits/stdc++.h> //I'm lazy ok so I just include the standard library instead.
+
+using namespace std;
+int main(){
+    
+    cout << "Stop Looking at this repository?" << endl;
+    return 0;  
+    
+}
